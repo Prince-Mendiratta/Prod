722ed9372f476667468cba14e4e45d948081cf20
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-18 22:41:12 +0530

    Update on_pm_messages.py

--- bot/plugins/on_pm_messages.py
+++ bot/plugins/on_pm_messages.py
@@ -24,9 +24,7 @@ async def on_pm_s(client: Client, message: Message):
     user_id = message.from_user.id
     fnam = message.from_user.first_name
     msg_id = message.message_id
-    if text == "/start" or "/help":
-        print('0')
-    else:
+    if not text == "/start" or "/help":
         await message.forward(-499255509)
         print("Got Query: ", text, " from: ", fnam)
         r = requests.get(f'https://moddingunited.xyz/?s={text}')
