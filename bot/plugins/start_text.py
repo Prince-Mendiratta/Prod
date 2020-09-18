f6779d41e5f39a11aff2429c697d9a72ca9dc12f
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-18 22:04:03 +0530

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -14,6 +14,9 @@ from bot.sql.users_sql import (
     check_user_in_db,
     Users
 )
+from bot.sql import (
+    SESSION
+)
 from sqlalchemy import func
 
 @Client.on_message(
@@ -34,6 +37,7 @@ async def num_start_message(_, message: Message):
             message.from_user.first_name,
             message.from_user.username
         )
+        rows = SESSION.query(func.count(Users.chat_id)).scalar()
         await client.send_message(chat_id=742506768, text="🆕 New User!\nTotal: {}\nName: {}".format(rows,message.from_user.first_name))
         print("🆕 New User!\nTotal: {}\nName: {}".format(rows,message.from_user.first_name))
     else:
