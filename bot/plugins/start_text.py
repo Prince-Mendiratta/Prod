a2529b0b12a4af402f3d9ff8379ee06944e6ee60
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-18 21:57:30 +0530

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -28,13 +28,12 @@ async def num_start_message(_, message: Message):
     )
     q = check_user_in_db(message.from_user.id)
     print(q)
-    if q == True:
+    if q == False:
         add_user_to_db(
             message.from_user.id,
             message.from_user.first_name,
             message.from_user.username
         )
-        rows = session.query(func.count(Users.chat_id)).scalar()
         await client.send_message(chat_id=742506768, text="🆕 New User!\nTotal: {}\nName: {}".format(rows,message.from_user.first_name))
         print("🆕 New User!\nTotal: {}\nName: {}".format(rows,message.from_user.first_name))
     else:
