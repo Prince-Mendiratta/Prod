57429528bcf2cb89823a0db5ad4ded02f1375aaf
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-18 21:48:58 +0530

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -28,7 +28,7 @@ async def num_start_message(_, message: Message):
     )
     q = check_user_in_db(message.from_user.id)
     print(q)
-    if pp == False:
+    if q == True:
         add_user_to_db(
             message.from_user.id,
             message.from_user.first_name,
