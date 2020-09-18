115325360c8ba6a67c0bf05b6a2f33f5c1e27722
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-19 01:13:44 +0530

    Update in_group.py

--- bot/plugins/in_group.py
+++ bot/plugins/in_group.py
@@ -11,7 +11,7 @@ from request import get_mod
 
 @Client.on_message(
     filters.group &
-    filters.command("mod", [“/”, “!”]),
+    filters.command("mod", ["/", "!"]),
     group = 3
 )
 async def group_wala(client: Client, message: Message):
