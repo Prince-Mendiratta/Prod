0d50ebc4e904cd078c384e80fec86ad56669ee70
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-19 01:11:31 +0530

    Create in_group.py

---
+++ bot/plugins/in_group.py
@@ -0,0 +1,18 @@
+from pyrogram import (
+    Client,
+    filters
+)
+from pyrogram.types import (
+    Message
+)
+from pyrogram.errors.exceptions import UserIsBlocked
+from request import get_mod
+
+
+@Client.on_message(
+    filters.group &
+    filters.command("mod", [“/”, “!”]),
+    group = 3
+)
+async def group_wala(client: Client, message: Message):
+    get_mod(client: Client, message: Message)
