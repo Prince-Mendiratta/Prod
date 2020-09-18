33227f9f625158da16546e12651cad354d6e8680
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-19 01:41:24 +0530

    Update on_pm_messages.py

--- bot/plugins/on_pm_messages.py
+++ bot/plugins/on_pm_messages.py
@@ -3,10 +3,16 @@ from pyrogram import (
     filters
 )
 from bot.plugins.request import get_mod
+from pyrogram.types import Message
+
 
 
 @Client.on_message(
     filters.private
 )
 async def on_pm_s(client: Client, message: Message):
+    text = str(message.text)
+    user_id = message.from_user.id
+    fnam = message.from_user.first_name
+    msg_id = message.message_id
     get_mod(client, message)
