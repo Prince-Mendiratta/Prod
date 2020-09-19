61ca7bf5adb2557e126d59e71b0181d05024c124
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-09-19 11:21:21 +0530

    Update in_group.py

--- bot/plugins/in_group.py
+++ bot/plugins/in_group.py
@@ -18,7 +18,7 @@ async def group_wala(client: Client, message: Message):
     intro = str(message.text)
     cmds, query = intro.split(' ', 1)
     text = str(query)
-    user_id = message.from_user.id
+    user_id = message.chat.id
     fnam = message.from_user.first_name
     msg_id = message.message_id
     await get_mod(client, message, text, user_id, fnam, msg_id)
