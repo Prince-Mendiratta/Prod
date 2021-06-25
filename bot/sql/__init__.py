76fea850cec8cd8b2694b5afb7162c045023f26a
Prince Mendiratta <prince.mendi@gmail.com>
2021-06-25 13:51:57 +0530

    Update __init__.py

--- bot/sql/__init__.py
+++ bot/sql/__init__.py
@@ -9,7 +9,7 @@ from .. import DB_URI
 def start() -> scoped_session:
     """ returns SQLAlchemy ScopedSession """
     if DB_URI.startswith("postgres://"):
-        DB_URI.replace("postgres://", "postgresql://", 1)
+        DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
     engine = create_engine(DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
