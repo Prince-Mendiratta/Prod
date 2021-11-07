9f9b3631f050eb4de42706bc861f2b322b2954f2
Prince Mendiratta <prince.mendi@gmail.com>
2021-11-07 19:25:12 +0530

    test
    Signed-off-by: Prince Mendiratta <prince.mendi@gmail.com>

--- bot/hf/request.py
+++ bot/hf/request.py
@@ -29,7 +29,7 @@ async def get_mod(client: Client, message: Message, text, user_id, fnam, msg_id)
         except UserIsBlocked or ChatWriteForbidden or ChatSendMediaForbidden:
             print('blocked')
     soup = BeautifulSoup(r.content, 'html.parser')
-    h1 = soup.find('h1')
+    h1 = soup.find('h2')
     if "Nothing" in h1.text:
         try:
             await client.send_message(chat_id=user_id, text=f"⭕ Oops !! I didn't found any results on the keyword, {text}.. Please retry after checking the spellings or request the mod at @moddingunited_bot! We will upload it soon on our channel..", reply_to_message_id=msg_id)
